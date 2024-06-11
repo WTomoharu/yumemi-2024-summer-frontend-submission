246c07f64cf716c728498a5a46d5f5a6658f3b1e
Tomoharu Watanabe <tomoharu.dev@gmail.com>
2024-06-11 14:22:46 +0900

    eslint.config.jsをformat

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,14 @@
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import eslintConfigPrettier from 'eslint-config-prettier';
-import pluginVue from "eslint-plugin-vue";
-import vueParser from "vue-eslint-parser";
+import globals from "globals"
+import tseslint from "typescript-eslint"
+import eslintConfigPrettier from "eslint-config-prettier"
+import pluginVue from "eslint-plugin-vue"
+import vueParser from "vue-eslint-parser"
 
-export default [ 
+export default [
   {
-    languageOptions: { globals: globals.browser }
+    languageOptions: {
+      globals: globals.browser,
+    },
   },
   eslintConfigPrettier,
   ...tseslint.configs.recommended,
@@ -15,7 +17,10 @@ export default [
     files: ["*.vue", "**/*.vue"],
     languageOptions: {
       parser: vueParser,
-      parserOptions: { parser: tseslint.parser, sourceType: "module" },
+      parserOptions: {
+        parser: tseslint.parser,
+        sourceType: "module",
+      },
     },
   },
 ]
