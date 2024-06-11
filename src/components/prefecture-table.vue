1c2fdf4d1430fae6717a2a806c69fa8a188be87c
Tomoharu Watanabe <tomoharu.dev@gmail.com>
2024-06-11 14:23:55 +0900

    prefecture-tableを試作

---
+++ src/components/prefecture-table.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { api } from "../lib/api"
+
+type Prefecture = {
+  prefCode: number
+  prefName: string
+}
+
+const res = await api.get("api/v1/prefectures")
+const prefectures: Prefecture[] = res.data.result
+</script>
+
+<template>
+  <div class="grid">
+    <div v-for="prefecture in prefectures" :key="prefecture.prefCode">
+      <input type="checkbox" /> {{ prefecture.prefName }}
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.grid {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+}
+</style>
