0b83cd95365d5fe96306b0701124adfe68afe2d7
Tomoharu Watanabe <tomoharu.dev@gmail.com>
2024-06-11 13:15:36 +0900

    prettierを導入

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,13 @@
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import eslintConfigPrettier from 'eslint-config-prettier';
 import pluginVue from "eslint-plugin-vue";
 
 export default [ 
   {
     languageOptions: { globals: globals.browser }
   },
+  eslintConfigPrettier,
   ...tseslint.configs.recommended,
   ...pluginVue.configs["flat/essential"],
 ]
