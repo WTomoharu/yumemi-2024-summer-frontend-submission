64371b13674fef0904fa78771813fb6cd4d2a90e
Tomoharu Watanabe <tomoharu.dev@gmail.com>
2024-06-15 05:51:22 +0900

    mockによるテストを実装

--- test/components/prefecture-chart.test.ts
+++ test/components/prefecture-chart.test.ts
@@ -2,11 +2,12 @@
  * @vitest-environment jsdom
  */
 
-import { describe, expect, test } from "vitest"
+import { describe, expect, test, vi } from "vitest"
 import { defineComponent } from "vue"
 import { flushPromises, mount } from "@vue/test-utils"
 
 import PrefectureChart from "../../src/components/prefecture-chart.vue"
+import { Population, fetchPopulation } from "../../src/lib/population"
 
 const SuspensePrefectureChart = defineComponent({
   name: "SuspensePrefectureChart",
@@ -25,5 +26,27 @@ describe("PrefectureChart", () => {
 
     // 生成されたTop Elementのクラスを確認
     expect(section.classList).toContain("chart-section")
+  })
+
+  test("Mock Test", async () => {
+    const populations = await vi.hoisted(async () => {
+      const { ref } = await import("vue")
+      return ref<Population[]>([])
+    })
+
+    vi.mock("../../src/store/population.ts", () => ({
+      populations,
+    }))
+
+    const wrap = mount(SuspensePrefectureChart)
+    const component = wrap.findComponent(PrefectureChart)
+    const section = component.find(".chart-section").element as HTMLElement
+
+    expect(section.style.getPropertyValue("display")).toBe("none")
+
+    populations.value.push(await fetchPopulation(1))
+    await flushPromises()
+
+    expect(section.style.getPropertyValue("display")).toBe("")
   })
 })
--- test/components/prefecture-table.test.ts
+++ test/components/prefecture-table.test.ts
@@ -2,7 +2,7 @@
  * @vitest-environment jsdom
  */
 
-import { describe, expect, test } from "vitest"
+import { describe, expect, test, vi } from "vitest"
 import { defineComponent } from "vue"
 import { flushPromises, mount } from "@vue/test-utils"
 
@@ -28,5 +28,30 @@ describe("PrefectureTable", () => {
 
     // 47都道府県が生成されているかを確認
     expect(section.getElementsByClassName("table-cell").length).toBe(47)
+  })
+
+  test("Mock Test", async () => {
+    const addPopulation = vi.hoisted(() => vi.fn())
+    const removePopulation = vi.hoisted(() => vi.fn())
+
+    vi.mock("../../src/store/population.ts", () => ({
+      addPopulation,
+      removePopulation,
+    }))
+
+    const wrap = mount(SuspensePrefectureTable)
+
+    await flushPromises()
+
+    const component = wrap.findComponent(PrefectureTable)
+    const cell = component.find<HTMLInputElement>(".table-cell input")
+
+    // チェックボックスのチェックを付けたとき、addPopulationが呼ばれるか確認
+    cell.setValue(true)
+    expect(addPopulation).toHaveBeenCalledTimes(1)
+
+    // チェックボックスのチェックを外したとき、removePopulationが呼ばれるか確認
+    cell.setValue(false)
+    expect(removePopulation).toHaveBeenCalledTimes(1)
   })
 })
