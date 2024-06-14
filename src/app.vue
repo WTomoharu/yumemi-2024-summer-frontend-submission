0799ab75af10353d65e6ac03372aad67e55206f7
Tomoharu Watanabe <tomoharu.dev@gmail.com>
2024-06-14 21:53:03 +0900

    appを作成

--- src/app.vue
+++ src/app.vue
@@ -1,9 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import PrefectureGraph from "./components/prefecture-graph.vue"
+import PrefectureTable from "./components/prefecture-table.vue"
+</script>
 
 <template>
-  <div>
-    <h1>Hello World</h1>
-  </div>
+  <h1>総人口推移グラフ（都道府県別）</h1>
+  <Suspense>
+    <PrefectureTable />
+  </Suspense>
+  <Suspense>
+    <PrefectureGraph />
+  </Suspense>
 </template>
 
-<style scoped></style>
+<style scoped>
+h1 {
+  font-size: 1.5em;
+  text-align: center;
+}
+</style>
